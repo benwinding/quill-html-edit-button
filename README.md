d7f6ce8625582f3bdaafeb7d46c717b0a49651b8
benwinding <benwinding@users.noreply.github.com>
2020-09-01 23:05:09 +0000

    Deploying to gh-pages from  @ 2aa0f3f43788a45f33348ef33a173ebf63144c2a 🚀

---
+++ README.md
@@ -0,0 +1,73 @@
+# quill-html-edit-button
+
+<!-- [START badges] -->
+
+[![NPM Version](https://img.shields.io/npm/v/quill-html-edit-button.svg)](https://www.npmjs.com/package/quill-html-edit-button)
+[![License](https://img.shields.io/npm/l/quill-html-edit-button.svg)](https://github.com/benwinding/quill-html-edit-button/blob/master/LICENSE)
+[![Downloads/week](https://img.shields.io/npm/dm/quill-html-edit-button.svg)](https://www.npmjs.com/package/quill-html-edit-button)
+[![Github Issues](https://img.shields.io/github/issues/benwinding/quill-html-edit-button.svg)](https://github.com/benwinding/quill-html-edit-button)
+
+<!-- [END badges] -->
+
+Quill.js Module which allows you to quickly view/edit the HTML in the editor
+
+![Demo](https://i.imgur.com/Gd5Pc6U.gif)
+
+- [Live Demo](https://benwinding.github.io/quill-html-edit-button/src/demo.html)
+- [Live Demo (using script tags)](https://benwinding.github.io/quill-html-edit-button/src/demo-script-tag.html)
+
+## Install
+
+`yarn add quill-html-edit-button`
+
+## Quickstart
+
+``` js
+import { htmlEditButton } from "./quill.htmlEditButton.js";
+
+Quill.register("modules/htmlEditButton", htmlEditButton);
+
+const quill = new Quill(editor, {
+  // ...
+  modules: {
+    // ...
+    htmlEditButton: {}
+  }
+});
+```
+
+## Quickstart (script tag)
+
+``` html
+    <script src="/dist/quill.htmlEditButton.min.js"></script>
+    <script>
+      Quill.register("modules/htmlEditButton", htmlEditButton);
+      const quill = new Quill(editor, {
+        // ...
+        modules: {
+          // ...
+          htmlEditButton: {}
+        }
+      });
+    </script>
+```
+
+## Options
+
+``` js
+modules: {
+  // ...
+  htmlEditButton: {
+    debug: true, // logging, default:false
+    msg: "Edit the content in HTML format", //Custom message to display in the editor, default: Edit HTML here, when you click "OK" the quill editor's contents will be replaced
+    okText: "Ok", // Text to display in the OK button, default: Ok,
+    cancelText: "Cancel", // Text to display in the cancel button, default: Cancel
+    buttonHTML: "&lt;&gt;", // Text to display in the toolbar button, default: <>
+    buttonTitle: "Show HTML source" // Text to display as the tooltip for the toolbar button, default: Show HTML source
+  }
+}
+```
+
+## Thanks
+
+This project is based on [quill-image-uploader](https://github.com/NoelOConnell/quill-image-uploader), thanks mate!
---
+++ src/quill.htmlEditButton.js
@@ -0,0 +1,178 @@
+import './styles.css';
+
+function $create(elName) {
+  return document.createElement(elName);
+}
+function $setAttr(el, key, value) {
+  return el.setAttribute(key, value);
+}
+
+let debug = false;
+const Logger = {
+  prefixString() {
+    return `</> quill-html-edit-button: `;
+  },
+  get log() {
+    if (!debug) {
+      return (...any) => {};
+    }
+    const boundLogFn = console.log.bind(console, this.prefixString());
+    return boundLogFn;
+  }
+};
+
+class htmlEditButton {
+  constructor(quill, options) {
+    debug = options && options.debug;
+    Logger.log("logging enabled");
+    // Add button to all quill toolbar instances
+    const toolbarModule = quill.getModule('toolbar');
+    if (!toolbarModule) {
+      throw new Error(
+        'quill.htmlEditButton requires the "toolbar" module to be included too'
+      );
+    }
+    this.registerDivModule();
+    let toolbarEl = toolbarModule.container;
+    const buttonContainer = $create("span");
+    $setAttr(buttonContainer, "class", "ql-formats");
+    const button = $create("button");
+    button.innerHTML = options.buttonHTML || "&lt;&gt;";
+    button.title = options.buttonTitle || "Show HTML source";
+    button.onclick = function(e) {
+      e.preventDefault();
+      launchPopupEditor(quill, options);
+    };
+    buttonContainer.appendChild(button);
+    toolbarEl.appendChild(buttonContainer);
+  }
+
+  registerDivModule() {
+    // To allow divs to be inserted into html editor
+    // obtained from issue: https://github.com/quilljs/quill/issues/2040
+    var Block = Quill.import('blots/block');
+    class Div extends Block {}
+    Div.tagName = "div";
+    Div.blotName = "div";
+    Div.allowedChildren = Block.allowedChildren;
+    Div.allowedChildren.push(Block)
+    Quill.register(Div);
+  }
+}
+
+function launchPopupEditor(quill, options) {
+  const htmlFromEditor = quill.container.querySelector(".ql-editor").innerHTML;
+  const popupContainer = $create("div");
+  const overlayContainer = $create("div");
+  const msg = options.msg || 'Edit HTML here, when you click "OK" the quill editor\'s contents will be replaced';
+  const cancelText = options.cancelText || "Cancel";
+  const okText = options.okText || "Ok";
+
+  $setAttr(overlayContainer, "class", "ql-html-overlayContainer");
+  $setAttr(popupContainer, "class", "ql-html-popupContainer");
+  const popupTitle = $create("i");
+  $setAttr(popupTitle, "class", "ql-html-popupTitle");
+  popupTitle.innerText = msg
+  const textContainer = $create("div");
+  textContainer.appendChild(popupTitle);
+  $setAttr(textContainer, "class", "ql-html-textContainer");
+  const textArea = $create("textarea");
+  $setAttr(textArea, "class", "ql-html-textArea");
+  textArea.value = formatHTML(htmlFromEditor);
+  const buttonCancel = $create("button");
+  buttonCancel.innerHTML = cancelText;
+  $setAttr(buttonCancel, "class", "ql-html-buttonCancel");
+  const buttonOk = $create("button");
+  buttonOk.innerHTML = okText;
+  const buttonGroup = $create("div");
+  $setAttr(buttonGroup, "class", "ql-html-buttonGroup");
+
+  buttonGroup.appendChild(buttonCancel);
+  buttonGroup.appendChild(buttonOk);
+  textContainer.appendChild(textArea);
+  textContainer.appendChild(buttonGroup);
+  popupContainer.appendChild(textContainer);
+  overlayContainer.appendChild(popupContainer);
+  document.body.appendChild(overlayContainer);
+
+  buttonCancel.onclick = function() {
+    document.body.removeChild(overlayContainer);
+  };
+  overlayContainer.onclick = buttonCancel.onclick;
+  popupContainer.onclick = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  buttonOk.onclick = function() {
+    const output = textArea.value.split(/\r?\n/g).map(el => el.trim());
+    const noNewlines = output.join("");
+    quill.container.querySelector(".ql-editor").innerHTML = noNewlines;
+    document.body.removeChild(overlayContainer);
+  };
+}
+
+// Adapted FROM jsfiddle here: https://jsfiddle.net/buksy/rxucg1gd/
+function formatHTML(code) {
+  "use strict";
+  let stripWhiteSpaces = true;
+  let stripEmptyLines = true;
+  const whitespace = " ".repeat(2); // Default indenting 4 whitespaces
+  let currentIndent = 0;
+  const newlineChar = "\n";
+  let prevChar = null;
+  let char = null;
+  let nextChar = null;
+
+  let result = "";
+  for (let pos = 0; pos <= code.length; pos++) {
+    prevChar = char;
+    char = code.substr(pos, 1);
+    nextChar = code.substr(pos + 1, 1);
+
+    const isBrTag = code.substr(pos, 4) === "<br>";
+    const isOpeningTag = char === "<" && nextChar !== "/" && !isBrTag;
+    const isClosingTag = char === "<" && nextChar === "/" && !isBrTag;
+    const isTagEnd = prevChar === ">" && char !== "<" && char !== " " && currentIndent > 0;
+    if (isBrTag) {
+      // If opening tag, add newline character and indention
+      result += newlineChar;
+      currentIndent--;
+      pos += 4;
+    }
+    if (isOpeningTag) {
+      // If opening tag, add newline character and indention
+      result += newlineChar + whitespace.repeat(currentIndent);
+      currentIndent++;
+    }
+    // if Closing tag, add newline and indention
+    else if (isClosingTag) {
+      // If there're more closing tags than opening
+      if (--currentIndent < 0) currentIndent = 0;
+      result += newlineChar + whitespace.repeat(currentIndent);
+    }
+    if(isTagEnd) {
+      result += newlineChar + whitespace.repeat(currentIndent);
+    }
+
+    // remove multiple whitespaces
+    else if (stripWhiteSpaces === true && char === " " && nextChar === " ")
+      char = "";
+    // remove empty lines
+    else if (stripEmptyLines === true && char === newlineChar) {
+      //debugger;
+      if (code.substr(pos, code.substr(pos).indexOf("<")).trim() === "")
+        char = "";
+    }
+
+    result += char;
+  }
+  Logger.log("formatHTML", {
+    before: code,
+    after: result
+  });
+  return result;
+}
+
+window.htmlEditButton = htmlEditButton;
+export default htmlEditButton;
+export { htmlEditButton };
