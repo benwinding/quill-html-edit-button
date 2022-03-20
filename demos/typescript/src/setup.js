81af792349a5fa884feeae90b9f8c2469340cb61
Ben Winding <hello@benwinding.com>
2022-03-20 15:19:31 +1100

    Typescript demo working

--- demos/typescript/src/setup.js
+++ demos/typescript/src/setup.js
@@ -1,6 +1,6 @@
 import Quill from "quill/core";
 
-import htmlEditButton from "../../quill.htmlEditButton";
+import htmlEditButton from "../../../src/quill.htmlEditButton";
 /* 
 import htmlEditButton from "quill-html-edit-button";
     ^ In production use this
