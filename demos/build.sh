de2d09574056530f27cec33ecf4d8eaceb921bf8
Ben Winding <hello@benwinding.com>
2022-03-20 22:58:27 +1100

    Fix demo build

--- demos/build.sh
+++ demos/build.sh
@@ -4,22 +4,22 @@ rm -rf ./dist
 mkdir dist
 
 # Build typescript
-pushd typescript
+cd typescript
 rm -rf ./dist
 yarn && yarn build
 cp -r ./dist ../dist/typescript
-popd 
+cd .. 
 
 # Build javascript
-pushd javascript
+cd javascript
 rm -rf ./dist
 yarn && yarn build
 cp -r ./dist ../dist/javascript
-popd
+cd ..
 
 # Build script tags
-pushd script-tags
+cd script-tags
 rm -rf ./dist
 mkdir ../dist/script-tags
 cp -r ./* ../dist/script-tags
-popd
+cd ..
