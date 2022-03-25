20630a9e239be9a275ced3b6269e58350ffba968
Ben Winding <hello@benwinding.com>
2022-03-25 17:44:56 +1100

    Case change fix

--- demos/vue/src/index.js
+++ demos/vue/src/index.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 
 import '@vueup/vue-quill/dist/vue-quill.bubble.css'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
