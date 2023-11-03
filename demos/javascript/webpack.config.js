b0d063619b426caeafa58ba8c83eb0e8b53f48b3
Ben Winding <hello@benwinding.com>
2023-11-03 19:09:04 +1100

    Remove `write-file-webpack-plugin` dep

--- demos/javascript/webpack.config.js
+++ demos/javascript/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const WriteFilePlugin = require('write-file-webpack-plugin');
 
 const isProd = process.argv.includes("production");
 
@@ -19,10 +18,12 @@ module.exports = {
     port: 8001,
     static: {
       directory: path.resolve(__dirname, "dist"),
-    }
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
   plugins: [
-    new WriteFilePlugin(),
     new CopyWebpackPlugin([
       { from: 'public', to: '.' },
       { from: '../header-text.js', to: '.' },
