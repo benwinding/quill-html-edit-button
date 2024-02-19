2db1bab694aa187788c6fdf9de450ea57067dda5
Ben Winding <hello@benwinding.com>
2024-02-19 13:52:33 +1100

    React demo working

--- demos/react/src/app.jsx
+++ demos/react/src/app.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
 
-// import htmlEditButton from "../../../src/quill.htmlEditButton";
+import htmlEditButton from "../../../src/quill.htmlEditButton";
 // import htmlEditButton from "quill-html-edit-button";
 //     ^ In production use this
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-// const modules = {
-//   ['name']: 'htmlEditButton',
-//   ['module']: htmlEditButton,
-//   ['options']: {
-//     debug: true
-//   },
-// };
+import Toolbar from "quill/modules/toolbar";
+import Snow from "quill/themes/snow";
+
+Quill.register({
+  'modules/toolbar': Toolbar,
+  'themes/snow': Snow,
+  "modules/htmlEditButton": htmlEditButton
+})
+
+const modules = {
+  htmlEditButton: {
+    debug: true,
+  }   
+};
 
 export function MyApp() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState('Here is some text!');
 
   return <ReactQuill theme="snow" value={value} onChange={setValue} 
-  // modules={modules} 
+  modules={modules} 
   />;
 }
